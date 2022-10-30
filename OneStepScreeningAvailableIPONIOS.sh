7f698f850ebf4ff10dc18115c60e679db3c0d2fa
whyick <101538160+whyick@users.noreply.github.com>
2022-10-30 13:10:03 +0800

    Update OneStepScreeningAvailableIPONIOS.sh

--- OneStepScreeningAvailableIPONIOS.sh
+++ OneStepScreeningAvailableIPONIOS.sh
@@ -1,4 +1,4 @@
-wget https://github.com/whyick/CloudflareIPScreening/blob/main/CloudflareST_linux_386.tar?raw=true
+wget https://github.com/whyick/CloudflareIPScreening/releases/download/CF/CloudflareST_linux_386.tar
 tar -zxf CloudflareST_linux_386.tar
 chmod +x CloudflareST
 ./CloudflareST
